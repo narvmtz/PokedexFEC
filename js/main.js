051f090029c2530a358ce13a86a7757fa37ceb58
Pablo Bagliere <pablo.bagliere2@gmail.com>
2020-10-26 13:56:57 -0300

    [add] Card of pokemon

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-import { getCard } from './structure.js';
+import { Stats, Info, Card, Types } from './structure.js';
 const URL = 'https://pokeapi.co/api/v2/';
-
+const Pokedex = document.querySelector('#pokemon');
 const Limint = 20;
 const Offset = 0;
-
+let info = [];
+let stats = [];
 async function getPokemon() {
   let pokemon = await fetch(`${URL}pokemon/1`);
   return pokemon.json();
 }
 async function main() {
-  const pokemon = await getPokemon();
-  console.log(pokemon);
-  console.log(pokemon.name);
-  console.log(pokemon.id);
-  console.log(pokemon.sprites.front_default);
-  console.log(pokemon.sprites.back_default);
-  pokemon.stats.forEach((status) => {
-    console.log(status.base_stat);
+  const PokemonTest = await getPokemon();
+  info[0] = PokemonTest.sprites.front_default;
+  info[1] = PokemonTest.sprites.back_default;
+  info[2] = PokemonTest.id;
+  info[3] = PokemonTest.name;
+  PokemonTest.stats.forEach((status, i) => {
+    stats[i] = status.base_stat;
   });
-  pokemon.types.forEach((type) => {
-    console.log(type.type.name);
-  });
+  let Pokemon = [];
+  Pokemon[0] = Info(info);
+  Pokemon[1] = Types(PokemonTest.types);
+  Pokemon[2] = Stats(stats);
+  const card = Card(Pokemon);
+  Pokedex.innerHTML += card;
 }
 
 main();
--- js/structure.js
+++ js/structure.js
@@ -1,50 +1,59 @@
-function getCard(stats, imgs, name, id, types) {
-  let htmlType = `<div class="type">`;
-  types.forEach((type) => {
-    htmlType = +`<p class= ${type} >${type}</p>`;
-  });
-  const card = `
-  <section class="card">
-  <div class="Info">
-    <img src=${imgs[0]} alt="front_default"
-      class="sprite">
-    <img src=${imgs[1]} alt="back_default"
-      class="sprite">
-    <p># <span class="number">${id}</span></p>
-  </div>
-  <h3>${name}</h3>
-  ${htmlType}
-  </div>
+function Stats(stats) {
+  return `
   <div class="stats">
     <span>
       <p><i class="fas fa-heart"></i> HP</p>
-      <p>${stats.hp}</p>
+      <p>${stats[0]}</p>
     </span>
     <span>
       <p><i class="fas fa-fire"></i> Attack</p>
-      <p>${stats.attack}</p>
+      <p>${stats[1]}</p>
     </span>
     <span>
       <p><i class="fas fa-shield-alt"></i> Defense</p>
-      <p>${stats.defense}</p>
+      <p>${stats[2]}</p>
     </span>
     <span>
       <p><i class="fas fa-atom"></i> Special Attack</p>
-      <p>${stats.special_attack}</p>
+      <p>${stats[3]}</p>
     </span>
     <span>
       <p><i class="fas fa-plus-square"></i>
         Special Defense</p>
-      <p>${stats.special_defence}</p>
+      <p>${stats[4]}</p>
     </span>
     <span>
       <p><i class="fas fa-bolt"></i> Speed</p>
-      <p>${stats.speed}</p>
+      <p>${stats[5]}</p>
     </span>
-  </div>
-</section>`;
-
-  return card;
+  </div>`;
+}
+function Card(params) {
+  let card = '<section class="card">';
+  params.forEach((param) => {
+    card += param;
+  });
+  return (card += '</section>');
 }
 
-export { getCard };
+function Types(params) {
+  let htmlType = `<div class="type"> `;
+  params.forEach((type) => {
+    htmlType += `<p class= ${type.type.name} >${type.type.name}</p>`;
+  });
+  return (htmlType += `</div>`);
+}
+
+function Info(params) {
+  return `
+  <div class="Info">
+  <img src=${params[0]} alt="front_default"
+    class="sprite">
+  <img src=${params[1]} alt="back_default"
+    class="sprite">
+  <p># <span class="number">${params[2]}</span></p>
+</div>
+<h3>${params[3]}</h3>
+`;
+}
+export { Stats, Info, Card, Types };
