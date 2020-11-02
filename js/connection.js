d5b8224287cd87f4984d1782517864d95df9b3ab
Pablo Bagliere <pablo.bagliere2@gmail.com>
2020-11-02 16:23:50 -0300

    [feature] Search by types

--- js/connection.js
+++ js/connection.js
@@ -1,6 +1,5 @@
 const URL = 'https://pokeapi.co/api/v2/';
 const Offset = 0;
-let allPokemon = [];
 let allTypes;
 
 async function getAllPokemon(Limint) {
@@ -8,49 +7,39 @@ async function getAllPokemon(Limint) {
     const res = await fetch(`${URL}pokemon/?offset=${Offset}&limit=${Limint}`);
     if (!res.ok) throw new Error('Error en la api');
     const resPokemon = await res.json();
-    for (let index = 0; index < Limint; index++) {
-      await getPokemon(resPokemon.results[index].url).then((pokemon) => {
-        allPokemon.push(pokemon);
+    return promiseFetch(resPokemon.results).then((data) => {
+      const allPokemon = [];
+      data.forEach((pokemon) => {
+        allPokemon.push(estrcuturePokemon(pokemon));
       });
-    }
-    return allPokemon;
+      return allPokemon;
+    });
   } catch (error) {
     console.error(error.message);
   }
 }
 
-async function getPokemon(url) {
-  try {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error('Pokemon no encontrado');
-    const data = await res.json();
-    const pokemon = {
-      img: [data.sprites.front_default, data.sprites.front_shiny],
-      id: data.id,
-      name: data.name,
-      stats: data.stats,
-      types: data.types,
-    };
-    return pokemon;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-async function getType(url) {
-  try {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error('Tipo no encontrado');
-    const data = await res.json();
-    for (let index = 0; index < data.names.length; index++) {
-      const name = data.names[index].language.name;
-      if (name === 'es') {
-        return data.names[index].name;
+async function promiseFetch(results, type) {
+  if (type) {
+    return Promise.all(results.map((data) => fetch(data.pokemon.url))).then(
+      (data) => {
+        return Promise.all(data.map((r) => r.json()));
       }
-    }
-  } catch (error) {
-    console.error(error.message);
+    );
   }
+  return Promise.all(results.map((data) => fetch(data.url))).then((data) => {
+    return Promise.all(data.map((r) => r.json()));
+  });
+}
+
+function estrcuturePokemon(pokemon) {
+  return {
+    img: [pokemon.sprites.front_default],
+    id: pokemon.id,
+    name: pokemon.name,
+    stats: pokemon.stats,
+    types: pokemon.types,
+  };
 }
 
 async function getTypes() {
@@ -58,21 +47,49 @@ async function getTypes() {
     return allTypes;
   }
   allTypes = new Object();
+  allTypes['Todos'] = 'Todos';
   try {
-    const res = await fetch(`${URL}type`);
+    const res = await fetch(`${URL}type/`);
+    const length = `${URL}type/`.length;
     if (!res.ok) throw new Error('Error en la api');
     const data = await res.json();
-    for (let index = 0; index < data.results.length; index++) {
-      await getType(data.results[index].url).then((type) => {
-        if (type != undefined) {
-          allTypes[data.results[index].name] = type;
-        }
+    let results = [];
+    data.results.map((data) => {
+      if (data.url.slice(length, data.url.length - 1) <= 1000) {
+        results.push(data);
+      }
+    });
+    return promiseFetch(results).then((e) => {
+      e.forEach((type) => {
+        type.names.map((e) => {
+          if (e.language.name == 'es') {
+            allTypes[type.name] = e.name;
+          }
+        });
       });
-    }
-    return allTypes;
+      return allTypes;
+    });
   } catch (error) {
     console.error(error.message);
   }
 }
 
-export { getAllPokemon, getTypes };
+async function getPokemonType(type, limit) {
+  try {
+    const res = await fetch(`${URL}type/${type}`);
+    if (!res.ok) throw new Error('Error en la api');
+    const data = await res.json();
+    const pokemons = data.pokemon.slice(0, limit);
+    return promiseFetch(pokemons, true).then((data) => {
+      const allPokemon = [];
+      data.forEach((pokemon) => {
+        allPokemon.push(estrcuturePokemon(pokemon));
+      });
+      return allPokemon;
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export { getAllPokemon, getTypes, getPokemonType };
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-import { getAllPokemon } from './connection.js';
-import { getAllCard } from './card.js';
+import { getAllPokemon, getPokemonType } from './connection.js';
+import { getAllCard, resetCard } from './card.js';
 import { peintType, peintCard } from './peint.js';
+const TypeHtml = document.querySelector('#Type');
 const limit = 20;
+const Cards = peintCard(limit);
 
 async function main() {
   await peintType();
-  const Cards = peintCard(limit);
   await getAllPokemon(limit)
     .then((pokemons) => {
-      getAllCard(pokemons, Cards);
+      getAllCard(pokemons, Cards, false);
     })
     .catch((error) => {
       console.log(error);
@@ -16,3 +17,21 @@ async function main() {
 }
 
 main();
+
+TypeHtml.addEventListener('change', async () => {
+  const selectedOption = TypeHtml.options[TypeHtml.selectedIndex];
+  resetCard(Cards, limit);
+  if (selectedOption.value === 'Todos') {
+    await getAllPokemon(limit)
+      .then((pokemons) => {
+        getAllCard(pokemons, Cards, false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return;
+  }
+  await getPokemonType(selectedOption.value, limit).then((pokemons) => {
+    getAllCard(pokemons, Cards, true);
+  });
+});
